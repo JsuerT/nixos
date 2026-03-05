dd16bd778a436cafed073b8e78424168bd4e9054
JsuerT <johanna.suermann@iu-study.org>
2026-03-05 22:29:21 +0100

    syntax gefixt

--- nixos-config/common/default.nix
+++ nixos-config/common/default.nix
@@ -1,53 +1,19 @@
-{ config, pkgs, ... }:
-
-{ nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
-
-  time.timeZone = "Europe/Berlin"; i18n.defaultLocale = "en_US.UTF-8";
-
-  # Tastatur-Layout für die Konsole (TTY)
-  console.keyMap = "de";
-
-  # Tastatur-Layout für X11/Desktop
-  services.xserver.xkb = { layout = "de"; variant = "";
-  };
-
-  # Unfree Software erlauben 
-  nixpkgs.config.allowUnfree = true;
-
-  users.users.ticco = { isNormalUser = true; description = "ticco"; extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-  programs.firefox.enable = true;
-
-  environment.systemPackages = with pkgs; [ vim git wget jetbrains.webstorm # Jetzt kannst du das Kommentarzeichen entfernen
-  ];
-}{ config, pkgs, ... }:
+{ config, pkgs, ... }: # <-- Nur hier ganz oben!
 
 {
-  # Alles, was beide Rechner teilen:
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # ... dein ganzer Inhalt (nix.settings, users, etc.)
 
-  boot.loader.systemd-boot.enable = true; boot.loader.efi.canTouchEfiVariables = true;
-
-  time.timeZone = "Europe/Berlin"; i18n.defaultLocale = "en_US.UTF-8";
-
-  # Dein User
-  users.users.ticco = { isNormalUser = true; description = "ticco"; extraGroups = [ "networkmanager" "wheel" ];
-  };
-
-  # Gemeinsame Programme
-  programs.firefox.enable = true; environment.systemPackages = with pkgs; [
-    vim git wget
-#    jetbrains.webstorm
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    wget
+    jetbrains.webstorm
   ];
 
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "";
+  };
 
-
-services.xserver.xkb = { layout = "de"; variant = "";
-}
-
-nixpkgs.config.allowUnfree = true;
-  # ... weitere gemeinsame Einstellungen (Networking, Sound etc.)
-}
+  nixpkgs.config.allowUnfree = true;
+} # <-- Das hier muss die ALLERLETZTE Zeile sein!
--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -69,6 +69,8 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+nixpkgs.config.allowUnfree = true; 
+
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. wget

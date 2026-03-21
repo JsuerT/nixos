6cd9f5452173960cd6ed7ed4a2d73831451f1b59
Jojo (Ticco) <johanna.suermann@iu-study.org>
2026-03-21 10:31:03 +0100

    mariadb

--- configuration.nix
+++ configuration.nix
@@ -6,115 +6,123 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
+      (builtins.fetchTarball {
+        # Master-Zweig für Kompatibilität mit deinem 25.11 System
+        url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+      } + "/nixos")      
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; #
+  networking.hostName = "nixos"; 
   
   # Enable networking
-  networking.networkmanager.enable = true; #
+  networking.networkmanager.enable = true; 
   
   # Set your time zone.
-  time.timeZone = "Europe/Berlin"; #
+  time.timeZone = "Europe/Berlin"; 
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8"; #
+  i18n.defaultLocale = "en_GB.UTF-8"; 
 
   i18n.extraLocaleSettings = { 
-    LC_ADDRESS = "de_DE.UTF-8"; #
-    LC_IDENTIFICATION = "de_DE.UTF-8"; #
-    LC_MEASUREMENT = "de_DE.UTF-8"; #
-    LC_MONETARY = "de_DE.UTF-8"; #
-    LC_NAME = "de_DE.UTF-8"; #
-    LC_NUMERIC = "de_DE.UTF-8"; #
-    LC_PAPER = "de_DE.UTF-8"; #
-    LC_TELEPHONE = "de_DE.UTF-8"; #
-    LC_TIME = "de_DE.UTF-8"; #
+    LC_ADDRESS = "de_DE.UTF-8"; 
+    LC_IDENTIFICATION = "de_DE.UTF-8"; 
+    LC_MEASUREMENT = "de_DE.UTF-8"; 
+    LC_MONETARY = "de_DE.UTF-8"; 
+    LC_NAME = "de_DE.UTF-8"; 
+    LC_NUMERIC = "de_DE.UTF-8"; 
+    LC_PAPER = "de_DE.UTF-8"; 
+    LC_TELEPHONE = "de_DE.UTF-8"; 
+    LC_TIME = "de_DE.UTF-8"; 
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true; #
+  services.xserver.enable = true; 
 
   # Enable the Plasma 6 Desktop Environment.
-  services.displayManager.sddm.enable = true; #
-  services.desktopManager.plasma6.enable = true; #
+  services.displayManager.sddm.enable = true; 
+  services.desktopManager.plasma6.enable = true; 
 
   # Configure keymap in X11
   services.xserver.xkb = { 
-    layout = "de"; #
-    variant = ""; #
+    layout = "de"; 
+    variant = ""; 
   };
 
   # Configure console keymap
-  console.keyMap = "de"; #
+  console.keyMap = "de"; 
 
   # Enable CUPS to print documents.
-  services.printing.enable = true; #
+  services.printing.enable = true; 
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false; #
-  security.rtkit.enable = true; #
+  services.pulseaudio.enable = false; 
+  security.rtkit.enable = true; 
   services.pipewire = {
-    enable = true; #
-    alsa.enable = true; #
-    alsa.support32Bit = true; #
-    pulse.enable = true; #
+    enable = true; 
+    alsa.enable = true; 
+    alsa.support32Bit = true; 
+    pulse.enable = true;
   };
 
   # Enable touchpad support.
-  services.xserver.libinput.enable = true; #
+  services.xserver.libinput.enable = true; 
 
   # Define a user account.
   users.users.ticco = { 
-    isNormalUser = true; #
-    description = "ticco"; #
-    extraGroups = [ "networkmanager" "wheel" ]; #
-    packages = with pkgs; [ #
-      # thunderbird
+    isNormalUser = true; 
+    description = "ticco"; 
+    extraGroups = [ "networkmanager" "wheel" ]; 
+    packages = with pkgs; [ 
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true; #
+  programs.firefox.enable = true; 
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true; #
+  nixpkgs.config.allowUnfree = true; 
+
+  services.mysql = {
+    enable = true; 
+    package = pkgs.mariadb;
+  };
 
   # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [ #
-    openjdk21 #
-    git #
-    (vim-full.customize { #
-      name = "vim"; #
+  environment.systemPackages = with pkgs; [ 
+    mariadb
+    openjdk21 
+    git 
+#    libreoffice
+    (vim-full.customize { 
+      name = "vim"; 
       vimrcConfig.packages.myVimPackage = {
-        start = with pkgs.vimPlugins; [ #
-          vim-airline #
-          vim-airline-themes #
-          nerdtree #
-          vim-fugitive #
-          vim-gitgutter #
-          vim-surround #
-          vim-commentary #
-          fzf-vim #
-          gruvbox #
-          vim-polyglot #
-          indentLine #
-          auto-pairs #
+        start = with pkgs.vimPlugins; [ 
+          vim-airline 
+          vim-airline-themes 
+          nerdtree 
+          vim-fugitive 
+          vim-gitgutter 
+          vim-surround 
+          vim-commentary 
+          fzf-vim 
+          gruvbox 
+          vim-polyglot 
+          indentLine 
+          auto-pairs 
         ];
       };
-      vimrcConfig.customRC = '' #
+      vimrcConfig.customRC = '' 
         syntax on
         set number
         set relativenumber
         set termguicolors
         set encoding=utf-8
         set shiftwidth=2
-        set expandtab #
+        set expandtab 
         set mouse=a
 
         " Theme Einstellungen
@@ -126,21 +134,49 @@
         let mapleader = " "
 
         " Strg + n öffnet den Dateibaum (NERDTree)
-        nnoremap <C-n> :NERDTreeToggle<CR> #
+        nnoremap <C-n> :NERDTreeToggle<CR> 
 
         " Leertaste + ff sucht nach Dateien
-        nnoremap <leader>ff :Files<CR> #
+        nnoremap <leader>ff :Files<CR> 
         
         " Leertaste + fg sucht in Git-Dateien
-        nnoremap <leader>fg :GFiles<CR> #
+        nnoremap <leader>fg :GFiles<CR> 
       '';
-    }) #
+    }) 
   ];
 
   # Enable programs and services
-  programs.nm-applet.enable = true; #
-  programs.mtr.enable = true; #
+  programs.nm-applet.enable = true; 
+  programs.mtr.enable = true; 
 
-  # This value determines the NixOS release from which the default settings for stateful data...
-  system.stateVersion = "24.11"; # (Geändert auf aktuelle stabile Version)
+  # Home Manager globale Einstellungen
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  # Home Manager Konfiguration für ticco
+  home-manager.users.ticco = { pkgs, ... }: {
+    imports = [
+      # Korrekter Import des Plasma-Manager Moduls ohne Flakes
+      (builtins.fetchTarball "https://github.com/nix-community/plasma-manager/archive/trunk.tar.gz" + "/modules")
+    ];
+
+    programs.plasma = {
+      enable = true;
+      shortcuts = {
+        "kwin" = {
+           "Switch to Desktop1" = "Meta+1"; 
+          "Switch to Desktop 2" = "Meta+2";
+          "Switch to Desktop 3" = "Meta+3";
+          "Window Maximize" = "Meta+Up";
+        };
+        "services/org.kde.konsole.desktop" = {
+          "_launch" = "Meta+Return";
+        };
+      };
+    };
+
+    home.stateVersion = "24.11"; 
+  };
+
+  system.stateVersion = "24.11"; 
 }
